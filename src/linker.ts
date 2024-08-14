abd08b273f923046d58666671d4106d3cfb32b4d
vprufus7 <vprufus7@gmail.com>
2024-08-14 16:25:02 -0400

    perf: update

--- src/linker.ts
+++ src/linker.ts
@@ -4,27 +4,30 @@ import * as path from "path";
 
 interface LinkGameTestTaskOptions {
   autoRunOnWorldStart?: boolean;
+  productionBuild?: boolean;
 }
 
 export function linkGameTestsTask(opts: LinkGameTestTaskOptions) {
-  const linkerFilePath = "scripts/linker.ts";
-  const linkerFileContents: string[] = [];
   return async () => {
-    const gameTestFiles = await glob("scripts/**/*.test.ts");
-    for (const gameTestFilePath of gameTestFiles) {
-      const relPath = path.relative("scripts", gameTestFilePath);
-      linkerFileContents.push(`import "./${relPath}";`);
-    }
-    if (opts.autoRunOnWorldStart) {
-      linkerFileContents.push(
-        `import { world } from "@minecraft/server";
+    const linkerFilePath = "scripts/linker.ts";
+    const linkerFileContents: string[] = [];
+    if (opts.productionBuild) {
+      const gameTestFiles = await glob("scripts/**/*.test.ts");
+      for (const gameTestFilePath of gameTestFiles) {
+        const relPath = path.relative("scripts", gameTestFilePath);
+        linkerFileContents.push(`import "./${relPath}";`);
+      }
+      if (opts.autoRunOnWorldStart) {
+        linkerFileContents.push(
+          `import { world } from "@minecraft/server";
       
       world.afterEvents.worldInitialize.subscribe(async () => {
         const overworld = world.getDimension("overworld");
         await overworld.runCommandAsync("gametest runset");
       });
       `
-      );
+        );
+      }
     }
     await fsp.writeFile(linkerFilePath, linkerFileContents.join("\n"));
   };
